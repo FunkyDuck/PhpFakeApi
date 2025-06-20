f7192dd2b9c875a023c9978e0ec2f17b57a53a89
Loïc @ Funkyduck <jacques_loic@funkyduck.be>
2025-06-20 23:27:30 +0200

    Update CORS

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@ require_once __DIR__ . "/../src/Constants.php";
 use Database\JsonDb as Db;
 use App\Constants;
 
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: */*");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 header('Content-Type: application/json');
