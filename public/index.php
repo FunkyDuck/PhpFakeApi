470737fb6420fec72f82af20dc21f009306c2cbc
Loïc @ Funkyduck <jacques_loic@funkyduck.be>
2025-06-20 23:51:36 +0200

    Update preflights CORS -

--- public/index.php
+++ public/index.php
@@ -7,6 +7,14 @@ require_once __DIR__ . "/../src/Constants.php";
 use Database\JsonDb as Db;
 use App\Constants;
 
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type");
+    http_response_code(200);
+    exit;
+}
+
 header("Access-Control-Allow-Origin: */*, *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
