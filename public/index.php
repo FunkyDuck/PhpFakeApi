3cbac818865a4f7dbd36ea62addf53f4591eece6
Loïc @ Funkyduck <jacques_loic@funkyduck.be>
2025-06-23 13:29:50 +0200

    Remove headers from public/index

--- public/index.php
+++ public/index.php
@@ -7,15 +7,15 @@ require_once __DIR__ . "/../src/Constants.php";
 use Database\JsonDb as Db;
 use App\Constants;
 
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
-header('Content-Type: application/json');
+// header("Access-Control-Allow-Origin: *");
+// header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+// header("Access-Control-Allow-Headers: Content-Type");
+// header('Content-Type: application/json');
 
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(200);
-    exit;
-}
+// if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+//     http_response_code(200);
+//     exit;
+// }
 
 error_log("PHP Fake API version " . Constants::VERSION . "\nCreated by Ginji@FunkyDuck.");
     
