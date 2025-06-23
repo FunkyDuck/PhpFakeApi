2adc15258e3552d00dd2b9271dd56bf6fa9b4f8d
Loïc @ Funkyduck <jacques_loic@funkyduck.be>
2025-06-23 14:53:12 +0200

    Remove all headers on php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-ini_set('display_errors', '0');
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
-header('Content-Type: application/json');
+// ini_set('display_errors', '0');
+// header("Access-Control-Allow-Origin: *");
+// header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+// header("Access-Control-Allow-Headers: Content-Type");
+// header('Content-Type: application/json');
 
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     http_response_code(200);
