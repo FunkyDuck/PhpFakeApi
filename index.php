24089612c3adbc219d2dd94cbfd9252998db9cd0
Loïc @ Funkyduck <jacques_loic@funkyduck.be>
2025-06-23 14:46:31 +0200

    Bug fix Cors origin

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 ini_set('display_errors', '0');
-header("Access-Control-Allow-Origin: */*, *");
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 header('Content-Type: application/json');
